0b497c817df61583921f583b6588696b42a6c5a1
refracta <58779799+refracta@users.noreply.github.com>
2023-05-17 09:20:39 +0900

    Implement CLI model execution logic

--- src/Starter.ts
+++ src/Starter.ts
@@ -2,12 +2,60 @@ import SocketClient from './client/SocketClient';
 import {DefaultSocketData} from './types/chameleon-controller';
 import DefaultSocketManager from './client/impl/manager/DefaultSocketManager';
 import DefaultSocketHandler from './client/impl/handler/DefaultSocketHandler';
+import path from 'path';
+import * as fs from 'fs';
+import {NetConnectOpts} from 'net';
+import {ExecutionData} from './types/chameleon-platform.common';
 
-const [host, port, historyId] = process.argv.slice(2);
+const controllerDirectory = path.dirname(process.execPath);
+const configPath = `${controllerDirectory}/config.json`;
+let options: NetConnectOpts;
+let historyId: number;
+let executionData: ExecutionData | undefined = undefined;
+const isConfigExist = fs.existsSync(configPath);
+const rawHistoryId = process.argv?.[4];
+if (!isConfigExist && rawHistoryId && !Number.isNaN(rawHistoryId)) {
+    const [host, rawPort, rawHistoryId] = process.argv.slice(2);
+    options = {
+        host,
+        port: parseInt(rawPort)
+    };
+    historyId = parseInt(rawHistoryId);
+} else {
+    const [nameId, inputPath, parametersPath, outputPath] = process.argv.slice(2);
+    if (!nameId) {
+        console.log('Use "chameleon [MODEL NAME ID]" or "chameleon [MODEL NAME ID] [PARAMETER PATH] [OUTPUT PATH]" or "chameleon [MODEL NAME ID] [INPUT PATH] [PARAMETERS PATH] [OUTPUT PATH]"');
+        process.exit(1);
+    }
+    const [username, uniqueName] = nameId.split('/');
+    if (nameId && inputPath && parametersPath && outputPath) {
+        if (!(fs.existsSync(inputPath) && fs.existsSync(parametersPath))) {
+            console.log('The file does not exist at path.');
+            process.exit(1);
+        }
+        executionData = {username, uniqueName, inputPath, parametersPath, outputPath};
+    } else if (nameId && parametersPath && outputPath) {
+        if (!(fs.existsSync(parametersPath) && fs.existsSync(outputPath))) {
+            console.log('The file does not exist at path.');
+            process.exit(1);
+        }
+        fs.closeSync(fs.openSync(`${controllerDirectory}/empty`, 'w'));
+        executionData = {username, uniqueName, inputPath: `${controllerDirectory}/empty`, parametersPath, outputPath};
+    } else if (nameId) {
+        executionData = {username, uniqueName};
+    } else {
+        console.log('Use "chameleon [MODEL NAME ID]" or "chameleon [MODEL NAME ID] [PARAMETER PATH] [OUTPUT PATH]" or "chameleon [MODEL NAME ID] [INPUT PATH] [PARAMETERS PATH] [OUTPUT PATH]"');
+        process.exit(1);
+    }
+    const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+    options = {host: config.host, port: config.port};
+    historyId = config.historyId;
+}
 
-const socketClient = new SocketClient<DefaultSocketData, DefaultSocketManager>({
-    host,
-    port: parseInt(port)
-}, new DefaultSocketManager());
-
-socketClient.addHandler(new DefaultSocketHandler(parseInt(historyId)));
+const socketClient = new SocketClient<DefaultSocketData, DefaultSocketManager>(options, new DefaultSocketManager());
+socketClient.addHandler(new DefaultSocketHandler({
+    config: {...options, historyId},
+    path: configPath,
+    isMainConnection: !isConfigExist,
+    executionData
+}));
--- src/client/impl/manager/DefaultSocketManager.ts
+++ src/client/impl/manager/DefaultSocketManager.ts
@@ -1,6 +1,13 @@
 import SocketManager from '../../manager/SocketManager';
 import {DefaultSocket} from '../../../types/chameleon-controller';
-import {SocketMessageType} from '../../../types/chameleon-platform.common';
+import {
+    ExecutionData,
+    SocketFileMessage, SocketFileReceiveEndMessage,
+    SocketFileWaitMessage,
+    SocketLaunchMessage,
+    SocketMessageType, SocketProcessEndMessage,
+    SocketTerminalMessage
+} from '../../../types/chameleon-platform.common';
 
 export default class DefaultSocketManager extends SocketManager {
     getClientSocket() {
@@ -11,23 +18,27 @@ export default class DefaultSocketManager extends SocketManager {
         this.getClientSocket().write(JSON.stringify(data) + '\0');
     }
 
-    sendLaunch(historyId: number) {
-        this.json({msg: SocketMessageType.LAUNCH, historyId});
+    sendLaunch(historyId: number, isMainConnection: boolean, executionData?: ExecutionData) {
+        this.json({msg: SocketMessageType.LAUNCH, historyId, isMainConnection, executionData} as SocketLaunchMessage);
     }
 
     sendFileWait() {
-        this.json({msg: SocketMessageType.FILE_WAIT});
+        this.json({msg: SocketMessageType.FILE_WAIT} as SocketFileWaitMessage);
     }
 
     sendTerminal(data: string) {
-        this.json({msg: SocketMessageType.TERMINAL, data});
+        this.json({msg: SocketMessageType.TERMINAL, data} as SocketTerminalMessage);
     }
 
     sendProcessEnd() {
-        this.json({msg: SocketMessageType.PROCESS_END});
+        this.json({msg: SocketMessageType.PROCESS_END} as SocketProcessEndMessage);
     }
 
     sendFile(fileSize: number) {
-        this.json({msg: SocketMessageType.FILE, fileSize});
+        this.json({msg: SocketMessageType.FILE, fileSize} as SocketFileMessage);
+    }
+
+    sendFileReceiveEnd() {
+        this.json({msg: SocketMessageType.FILE_RECEIVE_END} as SocketFileReceiveEndMessage);
     }
 }
